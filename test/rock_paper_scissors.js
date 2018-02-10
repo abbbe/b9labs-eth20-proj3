ebd790ab18c1f536744cd4770760bff76e18c0d5
Alexandre Bezroutchko <abb@gremwell.com>
2018-02-10 03:13:22 +0100

    added newchallenge-again tests

--- test/rock_paper_scissors.js
+++ test/rock_paper_scissors.js
@@ -14,10 +14,12 @@ var RockPaperScissors = artifacts.require("./RockPaperScissors.sol");
 var RpsBet = Object.freeze({ "Null": 0, "Rock": 1, "Paper": 2, "Scissors": 3 })
 
 contract('RockPaperScissors', function (accounts) {
-  const aliceBet = RpsBet.Rock; // , bet2 = RpsBet.Paper, bet3 = RpsBet.Scissors;
+  const aliceBet = RpsBet.Rock; 
   const aliceBetNonce = "0x1111111111111111111111111111111111111111";
-  // const nonce2 = "0x2222222222222222222222222222222222222222";
-  // const nonce3 = "0x3333333333333333333333333333333333333333";
+  const bobBet = RpsBet.Paper; 
+  const bobBetNonce = "0x2222222222222222222222222222222222222222";
+  const bobBet2 = RpsBet.Scissors; 
+  const bobBetNonce2 = "0x3333333333333333333333333333333333333333";
   const address0 = "0x0000000000000000000000000000000000000000";
   let rps, alice, bob, carol;
 
@@ -35,6 +37,8 @@ contract('RockPaperScissors', function (accounts) {
       .then(_instance => rps = _instance);
   });
 
+  it("should do hashing right");
+  
   it("should reject direct transaction with value", function () {
     return expectedException(
       () => rps.sendTransaction({ from: alice, value: 1, gas: 3000000 }),
@@ -48,11 +52,15 @@ contract('RockPaperScissors', function (accounts) {
   });
 
   describe("new challenge", function () {
-    let aliceBetHash;
+    let aliceBetHash, bobBetHash, bobBetHash2;
 
     beforeEach("reset the hashes", function () {
       return rps.hashThat(aliceBet, aliceBetNonce)
-        .then(_aliceBetHash => aliceBetHash = _aliceBetHash);
+        .then(_aliceBetHash => aliceBetHash = _aliceBetHash)
+        .then(() => rps.hashThat(bobBet, bobBetNonce))
+        .then(_bobBetHash => bobBetHash = _bobBetHash)
+        .then(() => rps.hashThat(bobBet2, bobBetNonce2))
+        .then(_bobBetHash2 => bobBetHash2 = _bobBetHash2);
     });
 
     it("should reject with 1 day minus 1 block timeout", function () {
@@ -114,12 +122,76 @@ contract('RockPaperScissors', function (accounts) {
             .then(([sender, amount, lastMatchBlock]) => {
               assert.strictEqual(sender, alice);
               assert.strictEqual(amount.toString(10), "1001");
-              // assert.strictEqual(lastMatchBlock.toString(10), "0");
+              // FIXME assert.strictEqual(lastMatchBlock.toString(10), "0");
               assert.strictEqual(
                 lastMatchBlock.toString(10),
                 web3.toBigNumber(172800).plus(txObject.receipt.blockNumber).toString(10));
             })
           ));
+    });
+
+    describe("again", function () {
+
+      beforeEach("accept for bob", function () {
+        return rps.newChallenge(bobBetHash, 172800, { from: bob, value: 1001, gas: 3000000 });
+      });
+
+      it("should reject existing hash with same parameters", function () {
+        return expectedException(
+          () => rps.newChallenge(bobBetHash, 172800, { from: bob, value: 1001, gas: 3000000 }),
+          3000000);
+      });
+
+      it("should reject existing hash with different timeout", function () {
+        return expectedException(
+          () => rps.newChallenge(bobBetHash, 172799, { from: bob, value: 1001, gas: 3000000 }),
+          3000000);
+      });
+
+      it("should reject existing hash with different bob", function () {
+        return expectedException(
+          () => rps.newChallenge(bobBetHash, 172800, { from: carol, value: 1001, gas: 3000000 }),
+          3000000);
+      });
+
+      it("should emit a single event on second challenge with different hash", function () {
+        return rps.newChallenge(bobBetHash2, 172800, { from: alice, value: 1001, gas: 3000000 })
+          .then(txObject => {
+            assert.strictEqual(txObject.logs.length, 1);
+            assert.strictEqual(txObject.logs[0].event, "LogChallenge");
+            assert.strictEqual(txObject.logs[0].args.alice, alice);
+            assert.strictEqual(txObject.logs[0].args.amount.toString(10), "1001");
+            assert.strictEqual(
+              txObject.logs[0].args.lastMatchBlock.toString(10),
+              web3.toBigNumber(172800).plus(txObject.receipt.blockNumber).toString(10));
+          });
+      });
+
+      it("should keep Weis in contract on second deposit with different hash", function () {
+        return rps.newChallenge(bobBetHash2, 172800, { from: bob, value: 1001, gas: 3000000 })
+          .then(txObject => web3.eth.getBalancePromise(rps.address))
+          .then(balance => assert.strictEqual(balance.toString(10), "2002"));
+      });
+
+      it("should update games[] on second deposit with different hash", function () {
+        return rps.games(bobBetHash2)
+          .then(([sender, amount, lastMatchBlock]) => {
+            assert.strictEqual(sender, address0);
+            assert.strictEqual(amount.toString(10), "0");
+            assert.strictEqual(lastMatchBlock.toString(10), "0");
+          })
+          .then(() => rps.newChallenge(bobBetHash2, 172800, { from: bob, value: 1001, gas: 3000000 })
+            .then(txObject => rps.games(bobBetHash2)
+              .then(([sender, amount, lastMatchBlock]) => {
+                assert.strictEqual(sender, bob);
+                assert.strictEqual(amount.toString(10), "1001");
+                // FIXME assert.strictEqual(lastMatchBlock.toString(10), "0");
+                assert.strictEqual(
+                  lastMatchBlock.toString(10),
+                  web3.toBigNumber(172800).plus(txObject.receipt.blockNumber).toString(10));
+              })
+            ));
+      });
     });
   })
 });
