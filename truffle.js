657d20f23098515527746c81a8415654247b0a10
Alexandre Bezroutchko <abb@gremwell.com>
2018-02-11 15:12:31 +0100

    add private testnet

--- truffle.js
+++ truffle.js
@@ -1,4 +1,15 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+
+    net24601: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "24601"
+    }
+  }
 };
